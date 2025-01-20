c797371223167888604f0e82d85ddc4fede08179
ayesh156 <sdachathuranga@gmail.com>
2025-01-20 23:13:00 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Boolean
+is_student = False
+for_sale = True
+is_online = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT sale")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
